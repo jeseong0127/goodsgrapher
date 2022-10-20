bfb3920609b92afa647977d50a9b1d91fb45e5f8
jeseong <mywptjd0127@naver.com>
2022-10-20 10:54:54 +0900

    Feat: url mapping log 추가

--- src/main/java/com/vitasoft/goodsgrapher/GoodsgrapherApplication.java
+++ src/main/java/com/vitasoft/goodsgrapher/GoodsgrapherApplication.java
@@ -2,8 +2,10 @@ package com.vitasoft.goodsgrapher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GoodsgrapherApplication {
 
     public static void main(String[] args) {
